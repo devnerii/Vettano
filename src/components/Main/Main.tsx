c70b8c3c96b1e8740c040816c5f13e0ca4ac5753
Daniel <dnnxzz70@gmail.com>
2024-09-22 20:48:13 -0300

    Changes to be committed: 	modified:   package.json 	deleted:    public/assets/img/images/banner-home-purple-default.jpg 	modified:   public/assets/img/images/banner-home-purple-desktop.jpg 	deleted:    public/assets/img/images/banner-home-purple-mobile.jpg 	deleted:    public/assets/img/images/banner-home-purple-tablet.jpg 	modified:   src/components/Main/Main.tsx

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,25 +6,18 @@ const Main = () => {
     <div className="container mx-auto">
       <div className="grid grid-cols-12">
         <div className="col-span-12 flex justify-center mt-10">
-          <div className="w-11/12 max-h-[300px] overflow-hidden rounded-2xl"> {/* Limita a altura do contêiner */}
-            <picture>
-              <source
-                media="(min-width: 1024px)"
-                srcSet="/assets/img/images/banner-home-purple-desktop.jpg"
-              />
-              <source
-                media="(min-width: 640px)"
-                srcSet="/assets/img/images/banner-home-purple-tablet.jpg"
-              />
-              <Image
-                src="/assets/img/images/banner-home-purple-mobile.jpg"
-                alt="Banner de Abertura da Competição"
-                layout="responsive" // Mantém responsivo
-                width={1000}  // Define a largura para responsividade
-                height={250}  // Altura ajustada menor
-                className="object-cover"
-              />
-            </picture>
+          {/* Adiciona flex e alinha ao final para forçar a imagem para baixo */}
+          <div className="w-11/12 max-h-[300px] overflow-hidden rounded-2xl flex items-end">
+            <Image
+              src="/assets/img/images/banner-home-purple-desktop.jpg"
+              alt="Banner de Abertura da Competição"
+              layout="responsive"
+              width={1172}
+              height={384}
+              quality={100}
+              objectFit="cover"
+              priority
+            />
           </div>
         </div>
       </div>
