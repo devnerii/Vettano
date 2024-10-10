1657d810f31993d162cdc256edbf5fd40952970d
Daniel <dnnxzz70@gmail.com>
2024-10-10 16:56:22 -0300

    new file:   public/assets/img/images/pantera-1.png 	new file:   public/assets/img/images/pantera-2.png 	modified:   src/components/Main/Games.tsx

--- src/components/Main/Games.tsx
+++ src/components/Main/Games.tsx
@@ -4,6 +4,7 @@ import { IoGameController } from "react-icons/io5";
 import { MdLeaderboard } from "react-icons/md";
 import Image from "next/image";
 import CustomButton from "./CustomButton"; // Ajuste o caminho conforme a estrutura do seu projeto
+import { MdOutlineMiscellaneousServices } from "react-icons/md";
 
 const Games: React.FC = () => {
   return (
@@ -144,7 +145,7 @@ const Games: React.FC = () => {
         </div>
 
         {/* Quatro Cards */}
-        <div className="col-span-12 flex justify-between items-center mt-4 relative space-x-4">
+        {/* <div className="col-span-12 flex justify-between items-center mt-4 relative space-x-4">
           <div className="w-full sm:w-1/4 h-64 bg-gradient-to-b from-[#1D1C49] to-[#323179] rounded-lg p-4 text-white">
             teste 1
           </div>
@@ -156,6 +157,71 @@ const Games: React.FC = () => {
           </div>
           <div className="w-full sm:w-1/4 h-64 bg-gradient-to-b from-[#1D1C49] to-[#323179] rounded-lg p-4 text-white">
             teste 4
+          </div>
+        </div> */}
+
+        {/* Título Jogos */}
+        <div className="col-span-12 flex justify-start items-center mt-6 relative">
+          <MdOutlineMiscellaneousServices className="text-[#ffa723] text-base sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-xl font-bold mr-2" />
+          <h1 className="text-white text-base sm:text-lg md:text-lg lg:text-lg xl:text-lg 2xl:text-xl font-bold">
+            Serviços
+          </h1>
+        </div>
+
+        {/* Loja do Vetttano e outro card */}
+        <div className="col-span-12 flex flex-col sm:flex-row justify-start items-center mt-4 relative space-y-4 sm:space-y-0 sm:space-x-4">
+          {/* Card Loja do Vetttano */}
+          <div className="w-full sm:w-1/2 h-64 bg-gradient-to-b from-[#1D1C49] to-[#323179] rounded-lg p-4 text-white mr-2 px-12 py-28 relative overflow-hidden">
+            <h1 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-semibold relative z-20">
+              Loja do Vettano
+            </h1>
+            <CustomButton
+              ariaLabel="Jogue agora o modo de jogo Clássico"
+              className="hover:shadow-[0_0_30px_rgba(96,108,245,0.8),0_0_40px_rgba(93,82,238,0.6)] relative z-20"
+            >
+              Explorar
+            </CustomButton>
+
+            {/* Container da imagem com glow azul */}
+            <div className="absolute top-1/2 left-24 transform -translate-y-1/2 w-full h-full scale-100 z-10 pointer-events-none">
+              {/* Glow Azul */}
+              <div className="ml-24 mt-16 absolute inset-0 bg-purple-600 opacity-50 blur-3xl transform translate-x-4 md:translate-x-8 pointer-events-none"></div>
+
+              {/* Imagem da Pantera */}
+              <Image
+                src="/assets/img/images/pantera-1.png"
+                alt="Pantera com Dourado"
+                layout="fill"
+                objectFit="contain"
+                className="rounded-lg relative z-10 pointer-events-none mt-2 ml-4"
+              />
+            </div>
+          </div>
+
+          {/* Assinatura Vip Card */}
+          <div className="w-full sm:w-1/2 h-64 bg-gradient-to-b from-[#1D1C49] to-[#323179] rounded-lg p-4 text-white mr-2 px-12 py-28 relative overflow-hidden">
+            <h1 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-semibold relative z-20">
+              Assinatura Vip
+            </h1>
+            <CustomButton
+              ariaLabel="Jogue agora o modo de jogo Rápido"
+              className="hover:shadow-[0_0_30px_rgba(96,108,245,0.8),0_0_40px_rgba(93,82,238,0.6)] relative z-20"
+            >
+              Acessar
+            </CustomButton>
+            <div className="absolute top-1/2 left-24 transform -translate-y-1/2 w-full h-full scale-100 z-10 pointer-events-none">
+              {/* Glow Azul */}
+              <div className="ml-24 mt-16 absolute inset-0 bg-purple-600 opacity-80 blur-3xl transform translate-x-4 md:translate-x-8 pointer-events-none"></div>
+
+              {/* Imagem da Pantera */}
+              <Image
+                src="/assets/img/images/pantera-2.png"
+                alt="Descrição da imagem"
+                layout="fill"
+                objectFit="contain"
+                className="rounded-lg relative z-10 pointer-events-none ml-2 mt-2"
+              />
+            </div>
           </div>
         </div>
 
