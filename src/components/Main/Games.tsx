321466d0bba4f763682e01b080c92df9d945a548
Daniel <dnnxzz70@gmail.com>
2024-10-10 13:05:04 -0300

    Changes to be committed: 	deleted:    public/assets/img/images/cristais.svg 	new file:   public/assets/img/images/pantera2.png 	new file:   public/assets/img/images/tesouro.png 	modified:   src/components/Main/Games.tsx

--- src/components/Main/Games.tsx
+++ src/components/Main/Games.tsx
@@ -60,16 +60,16 @@ const Games: React.FC = () => {
             </CustomButton>
             <div className="absolute top-1/2 left-24 transform -translate-y-1/2 w-full h-full scale-100 z-10 pointer-events-none">
               {/* Glow Azul */}
-              <div className="ml-24 mt-16 absolute inset-0 bg-blue-400 opacity-50 blur-3xl transform translate-x-4 md:translate-x-8 pointer-events-none"></div>
+              <div className="ml-24 mt-16 absolute inset-0 bg-blue-400 opacity-80 blur-3xl transform translate-x-4 md:translate-x-8 pointer-events-none"></div>
 
               {/* Imagem da Pantera */}
-              {/* <Image
-                src="/caminho/para/imagem.png"
+              <Image
+                src="/assets/img/images/pantera2.png"
                 alt="Descrição da imagem"
                 layout="fill"
                 objectFit="contain"
                 className="rounded-lg relative z-10 pointer-events-none"
-              /> */}
+              />
             </div>
           </div>
         </div>
@@ -83,10 +83,9 @@ const Games: React.FC = () => {
               alt="Cristais"
               layout="fill"
               objectFit="cover"
-              quality={100}
+              quality={50}
               className="absolute inset-0 w-full h-full z-10 ml-10 mt-7"
             />
-
             <h1 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-semibold relative z-20">
               Modo Instantâneo
             </h1>
@@ -98,8 +97,27 @@ const Games: React.FC = () => {
             </CustomButton>
           </div>
 
-          <div className="w-full sm:w-1/3 h-64 bg-gradient-to-b from-[#1D1C49] to-[#323179] rounded-lg p-4 text-white">
-            teste 2
+          <div className="w-full sm:w-1/3 h-64 rounded-lg px-12 py-28 text-white relative bg-gradient-to-b from-[#1D1C49] to-[#323179] overflow-hidden">
+          <div className="ml-24 mt-28 absolute inset-0 bg-blue-700 opacity-90 blur-3xl transform translate-x-4 md:translate-x-8 pointer-events-none"></div>
+            {/* Imagem de fundo */}
+            <Image
+              src="/assets/img/images/tesouro.png"
+              alt="Modo Livre Imagem"
+              layout="fill"
+              objectFit="contain"
+              quality={10}
+              className="absolute inset-0 w-full h-full z-10 scale-105 ml-24 mt-16"
+            />
+
+            <h1 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-semibold relative z-20">
+              Modo Livre
+            </h1>
+            <CustomButton
+              ariaLabel="Jogue agora o modo de jogo Instantâneo"
+              className="hover:shadow-[0_0_30px_rgba(96,108,245,0.8),0_0_40px_rgba(93,82,238,0.6)] relative z-20"
+            >
+              Jogar
+            </CustomButton>
           </div>
           <div className="w-full sm:w-1/3 h-64 bg-gradient-to-b from-[#1D1C49] to-[#323179] rounded-lg p-4 text-white">
             teste 3
